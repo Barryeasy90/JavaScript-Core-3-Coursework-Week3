4caf9c70f18418e34e6763c9bcf69354af2743ee
Jihad7898 <107761291+Barryeasy90@users.noreply.github.com>
2022-11-15 07:21:03 +0000

    display using deconstruction is very tedious
    can any show the easy way to do this thanks

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,22 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+function inGryffindorHouse(profil){
+  console.log(profil.length)
+ const gryffindorHouse = []
+profil.find(e => {
+ if (e.house === "Gryffindor"){
+ gryffindorHouse.push(e)
+}
+})
+const [first, second, third, fourth, fifth] = gryffindorHouse
+const {firstName, lastName} = first // do we have to iterate all of it if yes! how? for using destruction is very tedious 
+console.log(firstName, lastName) // can you show me how to display them item easily. I am stuck 
+
+//  console.log(gryffindorHouse)
+// console.log(gryffindorHouse.length)
+}
+inGryffindorHouse(hogwarts );
+// let [a, b, c, d, e, f, j, h, k, l] =  hogwarts
+// const {firstName, lastName, house, pet} = l
+// console.log(pet)
